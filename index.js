5a13d40ff9704af1b41053a08b8a78817aea4143
Duknam Yoo <yoo2001818@gmail.com>
2015-05-01 00:31:00 +0900

    Complete DB integration

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var serveStatic = require('serve-static');
 
 var app = express();
 
-generator.prepare(function(nouns, verbs, adjs) {
+generator.prepare(function() {
   app.all('/generate/:pattern?', function(req, res, next) {
     var pattern = req.params.pattern || 'an';
     var results = [];
@@ -15,6 +15,42 @@ generator.prepare(function(nouns, verbs, adjs) {
     }
     res.type('text/plain');
     res.send(results.join('\n'));
+  });
+  app.get('/shorten', function(req, res, next) {
+    var original = req.query.url;
+    if(original == null) {
+      res.sendStatus(400);
+      return;
+    }
+    db.queryByUrl(original, function(originEntry, err) {
+      if(originEntry) {
+        res.send(config.domain+originEntry.shorten);
+        return;
+      }
+      generator.generatePatternUnique('-', 20, function(word, callback) {
+        db.query(word, function(entry, err) {
+          callback(!entry);
+        });
+      }, function(word) {
+        db.add(word, original, function(entry, err) {
+          if(err) {
+            res.sendStatus(500);
+            return;
+          }
+          res.send(config.domain+word);
+        });
+      });
+    });
+  });
+  app.get('/:shorten', function(req, res, next) {
+    var shorten = req.params.shorten;
+    db.query(shorten, function(entry, err) {
+      if(!entry) {
+        next();
+        return;
+      }
+      res.redirect(entry.original);
+    });
   });
   app.use(new serveStatic('./public'));
   app.listen(config.port);
